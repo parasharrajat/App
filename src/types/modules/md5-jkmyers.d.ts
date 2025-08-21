33c22a9c8a4f6acf878e027b82fa4712b6a189ef
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-08-21 18:36:11 +0530

    Install MD5 hash lib

---
+++ src/types/modules/md5-jkmyers.d.ts
@@ -0,0 +1,9 @@
+declare module 'md5-jkmyers' {
+    /**
+     * Computes the MD5 hash of the input string.
+     * @param input - The string to hash.
+     * @returns The MD5 hash as a hexadecimal string.
+     */
+    function md5(input: string): string;
+    export default md5;
+}
